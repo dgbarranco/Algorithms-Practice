00dc3c56f370eba2742a34ac77906043ec8194fc
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-19 19:39:30 -0700

    Create Main class to test LinkedList functionality
    Initial implementation of Main class to test LinkedList methods.

---
+++ src/linkedlist/Main.java
@@ -0,0 +1,19 @@
+package linkedlists;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list1 = new LinkedList();
+        list1.append(3);
+        list1.append(7);
+        list1.append(25);
+        list1.append(16);
+        list1.append(5);
+        list1.append(8);
+        list1.append(1);
+        System.out.println(list1);
+        // FILL IN CODE: call other methods here to test your code
+
+
+
+    }
+}
